e7c03809c827442888bb2de58db07670ef282ad7
Bruno Kühnen Meneguello <bruno@meneguello.com>
2015-05-22 11:39:55 -0300

    Additional resources

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # code-services-api
 Coding Services API Specification
+
+GET /projects
+
+GET /projects/<project_id>
+
+POST /projects/<project_id>/build
+
+GET /projects/<project_id>/resources
+
+GET /projects/<project_id>/resources/<resource_id>
+
+GET /projects/<project_id>/resources/<resource_id>/meta
+
+GET /projects/<project_id>/resources/<resource_id>/raw
+
+GET /projects/<project_id>/resources/<resource_id>/ast
--- mock-server.py
+++ mock-server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from md5 import md5
 from copy import deepcopy
 
@@ -32,6 +32,21 @@ projects_data = [
   }
 ]
 
+builds_data = [
+  {
+    'id': md5('sample-build-1').hexdigest(),
+    'status': 'scheduled'
+  },
+  {
+    'id': md5('sample2-build-2').hexdigest(),
+    'status': 'running'
+  },
+  {
+    'id': md5('sample2-build-1').hexdigest(),
+    'status': 'done'
+  }
+]
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -51,16 +66,27 @@ def projects():
   })
 
 def project_find(project_id):
-  return next(project for project in projects_data if project['id'] == project_id)
+  return find(project_id, projects_data)
+
+def find(element_id, collection):
+  return next(element for element in collection if element['id'] == element_id)
 
 @app.route("/projects/<project_id>")
 def project_get(project_id):
   return jsonify(project_simplify(project_find(project_id)))
 
+@app.route("/projects/<project_id>/build", methods=['POST'])
+def project_build(project_id):
+  print(request.get_json(silent=True))
+  return jsonify({
+    'id': md5('sample-build-1').hexdigest(),
+    'status': 'scheduled'
+  })
+
 @app.route("/projects/<project_id>/resources")
 def resources(project_id):
   return jsonify({
-    'items': project_find(project_id)['resources'] 
+    'items': project_find(project_id)['resources']
   })
 
 def resource_find(project_id, resource_id):
@@ -69,6 +95,19 @@ def resource_find(project_id, resource_id):
 @app.route("/projects/<project_id>/resources/<resource_id>")
 def resources_get(project_id, resource_id):
   return jsonify(resource_find(project_id, resource_id))
+
+@app.route("/builds")
+def builds():
+  return jsonify({
+    'items': builds_data
+  })
+
+@app.route("/builds/<build_id>")
+def build_get(build_id):
+  return jsonify(build_find(build_id))
+
+def build_find(build_id):
+  return find(build_id, builds_data)
 
 if __name__ == "__main__":
   app.run(debug=True)
